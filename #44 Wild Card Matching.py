6950e3d56e1303c25cdd939776606aa1c0bf5e71
daviscyl <daviscyl@gmail.com>
2019-06-23 16:59:20 -0700

    accepted O(n+m) solution

--- #44 Wild Card Matching.py
+++ #44 Wild Card Matching.py
@@ -10,10 +10,26 @@ s could be empty and contains only lowercase letters a-z. p could be empty and c
 a-z, and characters like ? or *.
 """
 
+from collections import defaultdict
+
 
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        return None
+        nexts = 1
+        end_bit = 1 << len(s)
+        d = defaultdict(int)
+        for i, c in enumerate(s):
+            d[c] |= 1 << i
+        for c in p:
+            if c == '*':
+                nexts = (end_bit << 1) - (nexts & -nexts)
+            elif c == '?':
+                nexts <<= 1
+            else:
+                nexts = (nexts & d[c]) << 1
+            if nexts == 0:
+                return False
+        return nexts & end_bit != 0
 
 
 # test solition
