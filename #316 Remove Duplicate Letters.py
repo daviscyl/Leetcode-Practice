1aba1cb46c8511083d10cf27104ac2b1e91c5427
daviscyl <daviscyl@gmail.com>
2019-06-23 18:14:44 -0700

    problem setup

---
+++ #316 Remove Duplicate Letters.py
@@ -0,0 +1,16 @@
+""" 316. Remove Duplicate Letters
+Given a string which contains only lowercase letters, remove duplicate letters so that every letter appear
+once and only once. You must make sure your result is the smallest in lexicographical order among all possible
+results.
+"""
+
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        return None
+
+
+# test solition
+solution = Solution()
+s = "cbacdcbc"
+print(solution.removeDuplicateLetters(s))
