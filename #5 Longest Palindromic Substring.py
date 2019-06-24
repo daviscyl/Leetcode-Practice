d2739fa3950fc2622d14f5fd4b9168aafa75efa4
daviscyl <daviscyl@gmail.com>
2019-06-23 23:23:09 -0700

    problem setup

---
+++ #5 Longest Palindromic Substring.py
@@ -0,0 +1,15 @@
+""" 5. Longest Palindromic Substring
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is
+1000.
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        return None
+
+
+# test solition
+solution = Solution()
+s = "cbacdcbc"
+print(solution.longestPalindrome(s))
